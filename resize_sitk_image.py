93c3f50df772c05c21949a7c72a1d6285082d055
Yanan Wu <46510946+eternity123456@users.noreply.github.com>
2022-05-10 08:35:23 +0800

    Create resize_sitk_image.py

---
+++ resize_sitk_image.py
@@ -0,0 +1,19 @@
+def resize_image_itk(itkimage, newSize, resamplemethod=sitk.sitkLinear):
+
+    resampler = sitk.ResampleImageFilter()
+    originSize = itkimage.GetSize()  # 原来的体素块尺寸
+    originSpacing = itkimage.GetSpacing()
+    newSize = np.array(newSize,float)
+    factor = originSize / newSize
+    newSpacing = originSpacing * factor
+    newSize = newSize.astype(np.int) #spacing肯定不能是整数
+    resampler.SetReferenceImage(itkimage)  # 需要重新采样的目标图像
+    resampler.SetSize(newSize.tolist())
+    resampler.SetOutputSpacing(newSpacing.tolist())
+    resampler.SetTransform(sitk.Transform(3, sitk.sitkIdentity))
+    resampler.SetInterpolator(resamplemethod)
+    itkimgResampled = resampler.Execute(itkimage)  # 得到重新采样后的图像
+    return itkimgResampled
+input_image = sitk.ReadImage(fn)
+re_img = resize_image_itk(input_image, (224, 224,input_image.GetSize()[2]), resamplemethod=sitk.sitkNearestNeighbor)
+img = sitk.GetArrayFromImage(re_img)
